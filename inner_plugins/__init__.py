8d081694156d7fcc51879d5dd41f3e58bae3269a
邓冲 <d137670874@163.com>
2025-04-27 11:19:38 +0800

    暴露TaskFilePlugin插件

--- inner_plugins/__init__.py
+++ inner_plugins/__init__.py
@@ -1,4 +1,5 @@
 from inner_plugins.report_plugin import ReportPlugin
+from inner_plugins.task_assignment_plugin import TaskFilePlugin
 from inner_plugins.xmind_test_case_plugin import XmindPlugin
 
-__all__ = ['XmindPlugin', 'ReportPlugin']
+__all__ = ['XmindPlugin', 'ReportPlugin', 'TaskFilePlugin']
